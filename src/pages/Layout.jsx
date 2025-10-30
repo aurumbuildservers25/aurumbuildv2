fd003f6d280550166591251fa55aef861490b117
aurumbuildservers25 <admin@aurumbuild.eu>
2025-10-30 11:53:18 +0100

    Update Layout.jsx

--- src/pages/Layout.jsx
+++ src/pages/Layout.jsx
@@ -5,7 +5,7 @@ import { Button } from "@/components/ui/button";
 import { createPageUrl } from "@/utils";
 import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from "@/components/ui/dropdown-menu";
 import { motion, AnimatePresence } from 'framer-motion';
-import Header from '@/components/Header';
+import Header from "./Header.jsx";
 
 const languages = [
   { code: 'en', name: 'English', flag: 'EN' },
