208212c4f0e606b3b22501cb99c9470348fb9cbb
aurumbuildservers25 <admin@aurumbuild.eu>
2025-10-31 10:32:13 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,20 @@
+// src/App.jsx
 import './App.css'
 import Pages from "@/pages/index.jsx"
 import { Toaster } from "@/components/ui/toaster"
+import React, { useState } from "react"
 
 function App() {
+  // 👇 this controls Industrial / Residential globally
+  const [division, setDivision] = useState('industrial')
+
   return (
     <>
-      <Pages />
+      {/* Pass division + setDivision down */}
+      <Pages division={division} setDivision={setDivision} />
       <Toaster />
     </>
   )
 }
 
-export default App 
+export default App
